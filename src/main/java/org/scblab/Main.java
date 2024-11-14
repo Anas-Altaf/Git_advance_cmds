438e8f1c6f558e7e08d9c790e3305e4496f949fe
Anas Altaf <f223639@cfd.nu.edu.pk>
2024-11-14 23:03:43 +0500

    Main Update to be Rebased

--- src/main/java/org/scblab/Main.java
+++ src/main/java/org/scblab/Main.java
@@ -1,12 +1,8 @@
 package org.scblab;
 
+// Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the application!");
-         greetUser();
-    }
-
-    public static void greetUser() {
-        System.out.println("Hello, User!");
+        System.out.println("Main branch update.");
     }
 }
