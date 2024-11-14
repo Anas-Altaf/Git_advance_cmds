f5a2f7e3e2272f6d9b053f8db8d039d376ba6ee1
umairaltaf982 <umairaltaf982@gmail.com>
2024-11-14 22:53:24 +0500

    Fix critical bug in Main class

--- src/main/java/org/scblab/Main.java
+++ src/main/java/org/scblab/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
          greetUser();
+         // somehow changed some code
     }
 
     public static void greetUser() {
