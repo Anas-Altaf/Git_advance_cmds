2a56a63a82d4db23cf98813b4c7f6fbc65c48d80
umairaltaf982 <umairaltaf982@gmail.com>
2024-11-14 11:39:42 +0500

    added main content

--- src/main/java/org/scblab/Main.java
+++ src/main/java/org/scblab/Main.java
@@ -2,6 +2,11 @@ package org.scblab;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the application!");
+        greetUser();
+    }
+
+    public static void greetUser() {
+        System.out.println("Hello, User!");
     }
 }
