cdc5c35d46bafe308bce6bc723d757b5f565a954
umairaltaf982 <umairaltaf982@gmail.com>
2024-11-14 22:39:47 +0500

    Commit 2

--- src/main/java/org/scblab/Login.java
+++ src/main/java/org/scblab/Login.java
@@ -2,6 +2,9 @@ package org.scblab;
 
 public class Login {
     public boolean authenticate(String username, String password) {
+        System.out.println("Authenticating user...");
         return username.equals("admin") && password.equals("password123");
     }
 }
+
+
