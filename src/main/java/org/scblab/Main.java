20ff0955e2b2452cb97a92ef9879b3565cf46f0d
umairaltaf982 <umairaltaf982@gmail.com>
2024-11-14 23:00:15 +0500

    changed main

--- src/main/java/org/scblab/Main.java
+++ src/main/java/org/scblab/Main.java
@@ -3,6 +3,7 @@ package org.scblab;
 // Main.java
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Feature A work in progress.");
         System.out.println("Main branch update.");
     }
 }
