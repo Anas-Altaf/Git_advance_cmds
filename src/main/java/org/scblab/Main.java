3ce9bc539e568a10e5d49c889eb7bd86172952f8
Anas Altaf <f223639@cfd.nu.edu.pk>
2024-11-14 22:24:10 +0500

    Revert "added main content"
    This reverts commit 2a56a63a82d4db23cf98813b4c7f6fbc65c48d80.

--- src/main/java/org/scblab/Main.java
+++ src/main/java/org/scblab/Main.java
@@ -3,10 +3,11 @@ package org.scblab;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
-        // greetUser();
+         greetUser();
     }
 
     public static void greetUser() {
         System.out.println("Hello, User!");
+        System.out.println("Hello world!");
     }
 }
